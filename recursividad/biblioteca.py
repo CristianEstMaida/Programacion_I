1bac518004c79e3f0474e1693b699bab9c079dee
CristianEstMaida <55913935+CristianEstMaida@users.noreply.github.com>
2024-11-03 19:56:30 -0300

    Update biblioteca.py

--- recursividad/biblioteca.py
+++ recursividad/biblioteca.py
@@ -1,4 +1,4 @@
-def calcular_fibonacci(numero:str)->int:
+def calcular_fibonacci(numero:int)->int:
     '''
     La funcion calcula el fibonacci de un numero.
     Recibe el numero (str).
@@ -11,10 +11,20 @@ def calcular_fibonacci(numero:str)->int:
     return resultado
 
 def mostrar_serie_fibonacci(numero:int)->None:
+    '''
+    La funcion muestra la serie fibonacci.
+    Recibe un numero (int).
+    Retorna None.
+    '''
     for i in range(numero + 1):
         print(calcular_fibonacci(i))
 
 def determinar_suma_consecutiva(numero:int, x = 1)->bool:
+    '''
+    La funciona determina si el numero es resultado de la suma de numeros consecutivos.
+    Recibe un numero.
+    Retorna la True si es una suma consecutiva o False en caso contrario.
+    '''
     if x >= numero:
         resultado = False
     elif 2 * x + 1 == numero:
@@ -24,6 +34,11 @@ def determinar_suma_consecutiva(numero:int, x = 1)->bool:
     return resultado
 
 def calcular_factorial(numero:int)->int:
+    '''
+    La funcion calcula el factorial de un numero.
+    Recibe el numero (int).
+    Retorna el factorial.
+    '''
     if numero == 0:
         resultado = 1
     else:
